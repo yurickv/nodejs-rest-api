1cb20a59508848a4cdbb263dc555849304fc6535
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-09-15 14:36:28 +0300

    finish

--- helpers/index.js
+++ helpers/index.js
@@ -2,5 +2,12 @@ const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
 const validSchema = require("./validateSchema");
 const mongooseError = require("./mongooseError");
+const sendEmail = require("./sendEmail");
 
-module.exports = { HttpError, ctrlWrapper, validSchema, mongooseError };
+module.exports = {
+  HttpError,
+  ctrlWrapper,
+  validSchema,
+  mongooseError,
+  sendEmail,
+};
--- models/user.js
+++ models/user.js
@@ -23,6 +23,14 @@ const userSchema = new Schema(
     },
     token: String,
     avatarURL: { type: String, required: true },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
   },
   { versionKey: false },
   { timestamps: true }
@@ -36,6 +44,10 @@ const registerSchema = Joi.object({
   subscription: Joi.string(),
 });
 
+const emailSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+});
+
 const loginSchema = Joi.object({
   password: Joi.string().min(6).required(),
   email: Joi.string().pattern(emailRegexp).required(),
@@ -43,6 +55,7 @@ const loginSchema = Joi.object({
 
 const schemas = {
   registerSchema,
+  emailSchema,
   loginSchema,
 };
 
